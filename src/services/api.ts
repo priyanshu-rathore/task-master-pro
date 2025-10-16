f5b2fb65ad6b75e4b6a92ceb9b0d47a4e9334c88
priyanshu-rathore-slt <priyanshurathore132@gmail.com>
2025-10-16 15:14:19 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import type { IProject, ITask } from "../types";
 
-const API_BASE_URL =
-  import.meta.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL 
 
 // Create an Axios instance for consistent configuration
 const apiClient = axios.create({
